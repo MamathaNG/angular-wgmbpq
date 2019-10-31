cdbcad9b7b4dc4aef911ba4c2a1015ef2e2d9d26
MamathaNG <mamatha.ng123@gmail.com>
2019-10-31 15:27:03 +0530

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable(
   {
   providedIn: 'root'
@@ -7,6 +7,10 @@ import { Injectable } from '@angular/core';
 )
 export class CartService {
   items = [];
+
+  constructor(
+   private http: HttpClient
+  ) {}
 
   addToCart(product) {
     this.items.push(product);
